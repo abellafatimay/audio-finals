54b53576f0ce0142f13a1c62976c40523a00346a
May Fatima Abella <mayfatimabella@gmail.com>
2025-05-14 15:21:15 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'audio-finals',
+  webDir: 'www'
+};
+
+export default config;
